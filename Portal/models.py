e7000763f2775be3b72e2140d9a73726b2532938
SWIFT TALENT FORGE MARKETPLACE 2.0 <waterdrop198@gmail.com>
2025-05-04 14:40:46 +0300

    Update models.py

--- Portal/models.py
+++ Portal/models.py
@@ -54,8 +54,9 @@ class UsersCommunicationLanguage(models.Model):
 
     def __str__(self):
         return f"{self.user.user.username}/{self.language.language_name}/{self.level_of_fluency}"
+
 class Project(models.Model):
-    id = models.AutoField(primary_key=True, default=1)  # Example default value
+    id = models.AutoField(primary_key=True, default=1)
     project_name = models.CharField(max_length=100, unique=True)
     description = models.CharField(max_length=300, default=None)
     postedOn = models.DateTimeField(auto_now_add=True)
@@ -63,10 +64,11 @@ class Project(models.Model):
     isCompleted = models.BooleanField(default=False)
     deadline = models.DateField()
     task_count = models.IntegerField(default=0)
+    budget = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)  # Budget in USD
+    attachment = models.FileField(upload_to='project_attachments/', blank=True, null=True)
 
     def __str__(self):
         return self.project_name
-
 
 class Task(models.Model):
     task_name = models.CharField(max_length=50, primary_key=True)
@@ -140,4 +142,3 @@ class Notification(models.Model):
 
     def __str__(self):
         return f"From: {self._from.user.username}, To: {self._to.user.username}"
- 
